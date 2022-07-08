db8358c4ed8746905807e8f17de2b74e237937c0
Triana Yulianto <triana.yulianto@tonjoo.com>
2022-07-08 08:06:43 +0700

    Add base middleware

---
+++ src/Middleware.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Inertia;
+
+use Closure;
+use CodeIgniter\HTTP\Request;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Config\Services;
+
+class Middleware
+{
+    /**
+     * The root template that's loaded on the first page visit.
+     *
+     * @see https://inertiajs.com/server-side-setup#root-template
+     *
+     * @var string
+     */
+    protected $rootView = 'app';
+
+    /**
+     * Determines the current asset version.
+     *
+     * @see https://inertiajs.com/asset-versioning
+     *
+     * @param  Request  $request
+     * @return string|null
+     */
+    public function version(Request $request)
+    {
+        if (file_exists($manifest = './mix-manifest.json')) {
+            return md5_file($manifest);
+        }
+
+        return null;
+    }
+
+    /**
+     * Defines the props that are shared by default.
+     *
+     * @see https://inertiajs.com/shared-data
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function share(Request $request)
+    {
+        return [
+            'errors' => function () use ($request) {
+                return $this->resolveValidationErrors($request);
+            },
+        ];
+    }
+
+    /**
+     * Sets the root template that's loaded on the first page visit.
+     *
+     * @see https://inertiajs.com/server-side-setup#root-template
+     *
+     * @param  Request  $request
+     * @return string
+     */
+    public function rootView(Request $request)
+    {
+        return $this->rootView;
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  RequestInterface  $request
+     * @param null $arguments
+     *
+     * @return mixed
+     */
+    public function before(RequestInterface $request, $arguments = null) {
+        //
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  RequestInterface  $request
+     * @param  ResponseInterface  $response
+     * @param null $arguments
+     *
+     * @return mixed
+     */
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        $request = Services::request();
+
+        Inertia::version(function () use ($request) {
+            return $this->version($request);
+        });
+
+        Inertia::share($this->share($request));
+
+        Inertia::setRootView($this->rootView($request));
+
+        $response = $this->checkVersion($request, $response);
+
+        return $this->changeRedirectCode($request, $response);
+    }
+
+    /**
+     * In the event that the assets change, initiate a
+     * client-side location visit to force an update.
+     *
+     * @param  Request  $request
+     * @param  ResponseInterface  $response
+     * @return ResponseInterface
+     */
+    public function checkVersion(Request $request, ResponseInterface $response)
+    {
+        if ($request->header('X-Inertia') &&
+            $request->getMethod(true) === 'GET' &&
+            $request->header('X-Inertia-Version', '') !== Inertia::getVersion()
+        ) {
+            return Inertia::location((string) $request->getUri());
+        }
+
+        return $response;
+    }
+
+    /**
+     * Changes the status code during redirects, ensuring they are made as
+     * GET requests, preventing "MethodNotAllowedHttpException" errors.
+     *
+     * @param  Request  $request
+     * @param  ResponseInterface  $response
+     * @return ResponseInterface
+     */
+    public function changeRedirectCode(Request $request, ResponseInterface $response)
+    {
+        if ($request->header('X-Inertia') &&
+            $response->getStatusCode() === 302 &&
+            in_array($request->getMethod(true), ['PUT', 'PATCH', 'DELETE'])
+        ) {
+            $response->setStatusCode(303);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Resolves and prepares validation errors in such
+     * a way that they are easier to use client-side.
+     *
+     * @param  Request  $request
+     * @return object
+     */
+    public function resolveValidationErrors(Request $request)
+    {
+        $validation = \Config\Services::validation();
+
+        if (! $validation->listErrors()) {
+            return (object) [];
+        }
+
+        if ($request->header('x-inertia-error-bag')) {
+            return (object) [$request->header('x-inertia-error-bag') => $validation->getErrors()];
+        }
+
+        return (object) $validation->getErrors();
+    }
+}
